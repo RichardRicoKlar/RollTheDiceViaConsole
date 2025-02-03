f5d48df1f6c95bacfba1d5df64af1c3d8e21f45c
Richard Rico Klar <klar.richard@gmail.com>
2025-02-03 14:31:40 +0000

    Add project files.

---
+++ RollTheDice.Core/Randomiser.cs
@@ -0,0 +1,21 @@
+﻿namespace RollTheDice.Core
+{
+    public class Randomiser
+    {
+        public int MaxValue { get; set; }
+        public Randomiser(int value)
+        {
+            MaxValue = value + 1;
+        }
+        public int RollTheValue()
+        {
+            int finalValue = 0;
+            int minValue = 1;
+
+            Random random = new Random();
+
+            finalValue = random.Next(minValue,MaxValue);
+            return finalValue;
+        }
+    }
+}
---
+++ RollTheDiceConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using RollTheDice.Core;
+
+Console.ForegroundColor = ConsoleColor.White;
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Set the maximum roll first, then hit Enter to roll the virtual dice.");
+    Console.WriteLine();
+
+    try
+    {
+        Randomiser randomiser = new Randomiser(int.Parse(Console.ReadLine()));
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Rolling between 1 and " + (randomiser.MaxValue -1).ToString());
+            Console.WriteLine("I rolled number: " + randomiser.RollTheValue());
+            Console.WriteLine();
+            Console.Write("Hit");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(" Enter ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("to roll the dice.");
+            Console.ReadLine();
+        };
+    }
+    catch (IOException ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Please use numbers");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadLine();
+    }
+}
